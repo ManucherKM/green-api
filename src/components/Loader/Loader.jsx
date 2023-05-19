346ba458cfbbf636328777d37207c47cc8279822
015 <015@first.local>
2023-05-19 16:11:23 +0500

    .

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import Spinner from "../Spinner/Spinner";
+import classes from "./Loader.module.scss";
+
+const Loader = () => {
+  return (
+    <div className={classes.wrapper}>
+      <Spinner />
+    </div>
+  );
+};
+
+export default Loader;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,8 +2,12 @@ import classes from "./Login.module.scss";
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
 import { useState } from "react";
+import Loader from "../../components/Loader/Loader";
+import { useAuthStore } from "../../store/useAuthStore";
 
 const Login = () => {
+  const [loading, setLoading] = useState(false);
+
   const [form, setForm] = useState({
     idInstance: "",
     apiTokenInstance: "",
@@ -11,43 +15,53 @@ const Login = () => {
 
   const [error, setError] = useState(false);
 
+  const auth = useAuthStore((state) => state.auth);
+
   async function sendForm(e) {
     e.preventDefault();
+    setLoading(true);
 
     if (!form.apiTokenInstance || !form.idInstance) {
       setError(true);
       return;
     }
 
-    console.log(form);
+    auth(form.idInstance, form.apiTokenInstance);
+
+    setLoading(false);
   }
 
   return (
     <div className={classes.wrapper}>
       <div className={classes.form_wrapper}>
-        <h1>Авторизация</h1>
+        {loading ? (
+          <Loader />
+        ) : (
+          <>
+            <h1>Авторизация</h1>
+            <form>
+              <Input
+                onChange={(e) => {
+                  setForm((p) => ({ ...p, idInstance: e.target.value }));
+                }}
+                value={form.idInstance}
+                required
+                placeholder="idInstance"
+              />
+              <Input
+                value={form.apiTokenInstance}
+                required
+                onChange={(e) => {
+                  setForm((p) => ({ ...p, apiTokenInstance: e.target.value }));
+                }}
+                placeholder="apiTokenInstance"
+              />
 
-        <form>
-          <Input
-            onChange={(e) => {
-              setForm((p) => ({ ...p, idInstance: e.target.value }));
-            }}
-            value={form.idInstance}
-            required
-            placeholder="idInstance"
-          />
-          <Input
-            value={form.apiTokenInstance}
-            required
-            onChange={(e) => {
-              setForm((p) => ({ ...p, apiTokenInstance: e.target.value }));
-            }}
-            placeholder="apiTokenInstance"
-          />
-
-          {error && <h3>Некорректные данные</h3>}
-          <Button onClick={sendForm}>Отправить</Button>
-        </form>
+              {error && <h3>Некорректные данные</h3>}
+              <Button onClick={sendForm}>Отправить</Button>
+            </form>
+          </>
+        )}
       </div>
     </div>
   );
