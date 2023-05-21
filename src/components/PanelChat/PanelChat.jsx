7ab41a6fb98042a63390c3474aff1e43cc5ca63e
Manucher <manucher20056@gmail.com>
2023-05-21 16:42:03 +0500

    .

--- src/components/PanelChat/PanelChat.jsx
+++ src/components/PanelChat/PanelChat.jsx
@@ -31,7 +31,7 @@ const PanelChat = ({ currentChat }) => {
 			removeNotifications(res.receiptId)
 		}
 
-		setTimeout(checkUpdates, 500)
+		setTimeout(checkUpdates, 1000)
 	}
 
 	async function sendMessageHandler(e) {
